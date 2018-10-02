86b0959b5dc3866beff99ba3a873625a5827f0de
Monty Anderson <work@montyanderson.net>
2018-10-02 17:30:36 +0100

    Modules + example

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pipeline-image
+
 Image transform modules for pipeline
+
+``` javascript
+const { ImageReader, ImageCropper, ImageWriter } = require('pipeline-image');
+
+new Pipeline([
+	// raw images
+	new ImageReader(),
+	new ImageCropper(x, y, width, height),
+	new ImageWriter('image/jpeg')
+]);
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+const { TransformModule } = require('pipeline');
+const Jimp = require('jimp');
+
+const ImageReader = TransformModule.create(async image => {
+	return Jimp.read(image);
+});
+
+const ImageCropper = TransformModule.create(async (image, x, y, w, h) => {
+	image.crop(x - (w / 2), y - (h / 2), w, h);
+
+	return image;
+});
+
+const ImageWriter = TransformModule.create(async (image, format = 'image/jpeg') => {
+	return await util.promisify(image.getBuffer.bind(image))(format));
+});
+
+module.exports = {
+	ImageReader,
+	ImageCropper,
+	ImageWriter
+};
